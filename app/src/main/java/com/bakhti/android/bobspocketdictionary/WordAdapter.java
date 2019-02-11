dfa2b64ebf44e731ceae94f81584c7352d4711bf
Btagayaliev <32046645+Btagayaliev@users.noreply.github.com>
2019-02-11 01:24:25 +0100

    Update WordAdapter.java

--- app/src/main/java/com/bakhti/android/bobspocketdictionary/WordAdapter.java
+++ app/src/main/java/com/bakhti/android/bobspocketdictionary/WordAdapter.java
@@ -24,29 +24,21 @@ public class WordAdapter extends ArrayAdapter<Word> {
                     R.layout.list_view, parent, false);
         }
 
-        // Get the {@link AndroidFlavor} object located at this position in the list
+        
         Word currentWord = getItem(position);
 
-        // Find the TextView in the list_view.xml layout with the ID version_name
+        // Find the TextView with the ID
         TextView englishWord = (TextView) listItemView.findViewById(R.id.item1);
-        // Get the version name from the current AndroidFlavor object and
+       
         // set this text on the name TextView
         englishWord.setText(currentWord.getEnglishTranslation());
 
         // Find the TextView in the list_item.xml layout with the ID version_number
         TextView germanWord = (TextView) listItemView.findViewById(R.id.item2);
-        // Get the version number from the current AndroidFlavor object and
+      
         // set this text on the number TextView
         germanWord.setText(currentWord.getGermanTranslation());
-
-       /* // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-        // Get the image resource ID from the current AndroidFlavor object and
-        // set the image to iconView
-        iconView.setImageResource(currentAndroidFlavor.getImageResourceId());*/
-
-        // Return the whole list item layout (containing 2 TextViews and an ImageView)
-        // so that it can be shown in the ListView
+        
         return listItemView;
     }
 }
